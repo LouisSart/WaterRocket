9d86707da8b18d9ebf90f544d2380a6c4a218768
Épicier <louissarthou@gmail.com>
2025-10-16 16:22:21 +0200

    euler solve of the equation on the water level

---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Tank dimensions
+H = 1.0 # height of tank
+D = 0.3 # diameter of tank
+d = 0.02 # diameter of expulsion tube
+S = 3.1416 * (0.5 * D) ** 2 # cross section area of water tank
+s = 3.1416 * (0.5 * d) ** 2 # cross section area of expulsion tube
+beta = (S/s) ** 2 - 1 # convenient constant
+g = 9.81 # gravity acceleration
+rho = 10 ** 3 # water density
+Pa = 10 ** 5 # Atmospheric pressure
+P0 = 1 * Pa  # Initial tank pressure
+z0 = 0.5 * H # Inital water height
+V0 = (2 * g * z0 + 2 * (P0 - Pa) / rho) ** 0.5 # initial jet speed
+
+
+def p(z):
+    return (P0*z0)/(H-z)
+    
+def F(z):
+    return - np.sqrt((2. / beta) * ((g * z) + (p(z)-Pa)/rho))
+
+def stop(z, f, dt):
+    if z + dt * f < 0:
+        return True, "water level hit the bottom"
+    if abs(f) < 0.001:
+        return True, f"flow reached slow speed: {f} m/s"
+    if (g * z) + (p(z)-Pa)/rho < 0:
+        return True, f"hydrostatic equilibrium went wrong, reduce dt"
+    return False, ""
+
+dt = 0.001 * H / abs(F(z0))
+    
+def euler(tf):
+    ret = [z0]
+    z = z0
+    f = F(z0)
+    t = 0
+    stop_now = False
+    while not stop_now and t < tf:
+        # iterate first
+        ret.append(z + dt * f)
+        
+        # compute for loop stop condition
+        z = ret[-1]
+        f = F(z)
+        t += dt
+        stop_now, reason = stop(z, f, dt)
+    print("Simulation stopped because", reason)
+    return ret
+    
+z = euler(60)
+plt.plot(dt * np.arange(len(z)), z)
+plt.show()
