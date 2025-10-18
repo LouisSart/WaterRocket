8783047d2f92b1fff4e7d961297a609efccc8c8e
Épicier <louissarthou@gmail.com>
2025-10-18 15:33:19 +0200

    plot pressure instead of ejection speed

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ class Tank:
 
 InitialConditions = namedtuple("InitialConditions", ["P0", "z0"])
 
-def p(z, tank, ic):
+def pressure(z, tank, ic):
     return ic.P0 * (tank.H - ic.z0) / (tank.H - z)
     
 def F(z, tank, ic):
-    return - np.sqrt((2. / tank.beta) * ((g * z) + (p(z, tank, ic) - Pa) / rho))
+    return - np.sqrt((2. / tank.beta) * ((g * z) + (pressure(z, tank, ic) - Pa) / rho))
 
 def stop(z, f, tank, ic):
     if z < 0:
         return True, "water level hit the bottom"
     if abs(f) < 0.001:
         return True, f"flow reached slow speed: {abs(f)} m/s"
-    if (g * z) + (p(z, tank, ic) - Pa) / rho < 0:
+    if (g * z) + (pressure(z, tank, ic) - Pa) / rho < 0:
         return True, f"hydrostatic equilibrium went wrong, reduce dt"
     return False, ""
 
@@ -37,50 +37,54 @@ def stop(z, f, tank, ic):
 def euler(dt, tank, ic):
     z = [ic.z0]
     v = [abs(F(ic.z0, tank, ic) * tank.S / tank.s)]
+    p = [pressure(z[0], tank, ic)]
     stop_now, reason = stop(z[0], v[0], tank, ic)
     while not stop_now:
         zn = z[-1]
         f = F(zn, tank, ic)
         z.append(zn + dt * f)
         v.append(abs(f * tank.S / tank.s))
+        p.append(pressure(zn, tank, ic))
         stop_now, reason = stop(zn, f, tank, ic)
     print("Simulation stopped because", reason)
-    return z, v
+    return z, v, p
 
-def plot(z, v, tank, ic, water, flow, color=''):
+def plot(data, tank, ic, ax):
     PoP = "${\\frac{P_0}{P_a}}$"
     zoH = "${\\frac{z_0}{H}}$"
     soS = "${\\frac{s}{S}}$"
     simu_params = f"{PoP}={ic.P0/Pa}, {zoH}={ic.z0/tank.H:.2f}, {soS}={tank.S / tank.s:.2f}"
-    water_plot = water.plot(dt * np.arange(len(z)), z, label = simu_params)
-    flow_plot = flow.plot(dt * np.arange(len(v)), v, label = simu_params)
+    ax.plot(dt * np.arange(len(data)), data, label = simu_params)
 
 if __name__ == "__main__":
     # Simus
     tank = Tank(d = 0.02)
     ic = InitialConditions(P0 = 2. * Pa, z0 = 0.6 * tank.H)
     dt = 10**-4 * tank.H / abs(F(ic.z0, tank, ic))
-    z, v = euler(dt, tank, ic)
+    z, v, p = euler(dt, tank, ic)
     # ----------------------
     ic1 = InitialConditions(P0 = 2. * Pa, z0 = 0.5 * tank.H)
     dt = 10**-4  * tank.H / abs(F(ic1.z0, tank, ic1))
-    z1, v1 = euler(dt, tank, ic1)
+    z1, v1, p1 = euler(dt, tank, ic1)
     # ----------------------
     ic2 = InitialConditions(P0 = 2. * Pa, z0 = 0.4 * tank.H)
     dt = 10**-4  * tank.H / abs(F(ic2.z0, tank, ic2))
-    z2, v2 = euler(dt, tank, ic2)
+    z2, v2, p2 = euler(dt, tank, ic2)
 
     # Plot
-    f, (water, flow) = plt.subplots(2, 1, constrained_layout=True, figsize = (8, 8))
-    water.set_title("Water level (m)")
-    water.set_xlabel("t(s)", loc="right")
-    plot(z, v, tank, ic, water, flow)
-    plot(z1, v1, tank, ic1, water, flow, color = 'red')
-    plot(z2, v2, tank, ic2, water, flow, color = 'green')
-    flow.set_title("Ejection speed (m/s)")
-    flow.set_xlabel("t(s)", loc="right")
+    f, (w_ax, p_ax) = plt.subplots(2, 1, constrained_layout=True, figsize = (8, 8))
+    plot(z, tank, ic, w_ax)
+    plot(z1, tank, ic, w_ax)
+    plot(z2, tank, ic, w_ax)
+    plot(p, tank, ic, p_ax)
+    plot(p1, tank, ic, p_ax)
+    plot(p2, tank, ic, p_ax)
+    w_ax.set_title("Water level (m)")
+    w_ax.set_xlabel("t(s)", loc="right")
+    p_ax.set_title("Tank pressure (Pa)")
+    p_ax.set_xlabel("t(s)", loc="right")
 
     
-    water.legend()
-    flow.legend()
+    w_ax.legend()
+    p_ax.legend()
     plt.show()
